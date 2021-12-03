0f6d6b457d096e9d01f767522734f5f71a1ffc11
Machinero <piotr.chludzinski11@gmail.com>
2021-12-03 13:10:37 +0100

    Improved line drawing

---
+++ README.md
@@ -0,0 +1,19 @@
+# opengl-paint-test
+This is an example of drawing line with OpenGl.
+
+## Installation (linux)
+
+### Install dependencies:
+
+```
+sudo apt-get install mesa-common-dev freeglut3-dev libglew-dev libglm-dev
+```
+### Build and run:
+
+```bash
+mkdir build
+cd build
+cmake ..
+make
+./paint-gl
+```
--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,34 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <vector>
 
 struct Point {
   GLfloat x;
   GLfloat y;
 };
 
+GLuint ibo;
 GLuint vbo;
 GLuint program;
 GLint position;
 GLuint matrixID;
 glm::mat4 mvp;
-int bufferSize = 8 * 4096;
-
-int pointsSize = 0;
+unsigned int pointsSize = 0;
+unsigned int indiciesSize = 0;
+static constexpr int VERTEX_BUFFER_SIZE = 4096 * sizeof(Point);
+static constexpr int INDEX_BUFFER_SIZE = 4096 * sizeof(unsigned short);
+Point lastPoint;
 Point p;
+static unsigned short pointIndicies[2];
+bool mouseDownPressed = false;
+
+std::vector<Point> points;
+std::vector<unsigned int> indicies;
+
+#define POINT_XY(point) \
+  point.x, \
+  point.y  \
 
 std::string loadFile(std::string file) {
   std::ifstream input(file);
@@ -32,6 +45,21 @@ std::string loadFile(std::string file) {
 
   return {std::istreambuf_iterator<char>(input),
           std::istreambuf_iterator<char>()};
+}
+
+void debugDraw()
+{
+  std::cout << "Points\n";
+  for (auto x : points)
+  {
+    std::cout << x.x << " " << x.y << '\n';
+  }
+
+  std::cout << "Indicies\n";
+  for (auto x : indicies)
+  {
+    std::cout << x << '\n';
+  }
 }
 
 GLuint compileShader(GLuint type, const char *source) {
@@ -49,6 +77,47 @@ GLuint compileShader(GLuint type, const char *source) {
   }
 
   return shaderID;
+}
+
+void initBuffers() {
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, VERTEX_BUFFER_SIZE, nullptr, GL_DYNAMIC_DRAW);
+
+  glEnableVertexAttribArray(position);
+  glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, INDEX_BUFFER_SIZE, nullptr,
+               GL_DYNAMIC_DRAW);
+}
+
+void moveTo(int x, int y)
+{
+  p = {GLfloat(x), GLfloat(y)};
+  glBufferSubData(GL_ARRAY_BUFFER, pointsSize * sizeof(Point), sizeof(Point), &p);
+  points.push_back(p);
+
+  pointIndicies[1] = pointsSize;
+  indicies.push_back(pointIndicies[0]);
+  indicies.push_back(pointIndicies[1]);
+  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, indiciesSize * sizeof(unsigned short), sizeof(unsigned short) * 2, pointIndicies);
+  pointsSize++;
+  indiciesSize += 2;
+  pointIndicies[0] = pointIndicies[1];
+
+  std::cout << "Points: " << pointsSize << " Indicies: " << indiciesSize << '\n';
+  glutPostRedisplay();
+}
+
+void setPoint(int x, int y)
+{
+  p = {GLfloat(x), GLfloat(y)};
+  points.push_back(p);
+  glBufferSubData(GL_ARRAY_BUFFER, pointsSize * sizeof(Point), sizeof(Point), &p);
+  pointIndicies[0] = pointsSize;
+  pointsSize++;
 }
 
 GLuint createProgram(std::string vertexShaderPath,
@@ -103,25 +172,34 @@ bool init() {
   mvp = proj * view;
   matrixID = glGetUniformLocation(program, "MVP");
 
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, bufferSize, nullptr, GL_DYNAMIC_DRAW);
+  initBuffers();
+  // setPoint(500, 500);
+  // moveTo(600, 600);
+  // moveTo(600, 500);
+  // moveTo(700, 500);
+
+  // setPoint(300, 300);
+  // moveTo(300, 400);
+  // moveTo(350, 550);
+  // moveTo(0, 0);
+
   return true;
 }
 
 void mouse(int button, int state, int x, int y) {
   if (button == GLUT_LEFT_BUTTON) {
     if (state == GLUT_DOWN) {
+      std::cout << "Button down " << x << " " << y << '\n';
+      setPoint(x, y);
     }
   }
 }
 
 void mouseMotion(int mx, int my) {
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  p = {(GLfloat)mx, (GLfloat)my};
-  glBufferSubData(GL_ARRAY_BUFFER, pointsSize * sizeof(Point), 8, &p);
-  pointsSize++;
-  glutPostRedisplay();
+  // if (mouseDownPressed)
+  // {
+    moveTo(mx, my);
+  // }
 }
 
 void display() {
@@ -129,13 +207,12 @@ void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
   /* Draw using the vertices in our vertex buffer object */
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-  glEnableVertexAttribArray(position);
-  glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, 0, 0);
   glUniformMatrix4fv(matrixID, 1, GL_FALSE, &mvp[0][0]);
 
-  glDrawArrays(GL_LINE_STRIP, 0, pointsSize);
+  // glDrawArrays(GL_POINTS, 0, pointsSize);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glDrawElements(GL_LINES, indiciesSize, GL_UNSIGNED_SHORT, nullptr);
 
   glutSwapBuffers();
 }
